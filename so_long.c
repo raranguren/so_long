874852c39bae26611da08d1adb8dd1cb602885a1
rarangur <rarangur@student.42.fr>
2024-12-30 19:08:06 +0100

    hooks

--- so_long.c
+++ so_long.c
@@ -1,13 +1,48 @@
 #include "so_long.h"
 
-int	main(void)
+int	expose_hook(void *param)
 {
-	void	*mlx;
+	(void)param;
+	return ft_printf("expose_hook\n");
+}
 
+int	key_hook(int keycode, void *param)
+{
+	(void)param;
+	return ft_printf("key_hook %i\n", keycode);
+}
+
+int	mouse_hook(int keycode, void *param)
+{
+	(void)param;
+	return ft_printf("mouse_hook %i\n", keycode);
+}
+
+int	loop_hook(void *param)
+{
+	(void)param;
+	return ft_printf("loop_hook\n");
+}
+
+int	main(int argc, char **argv)
+{
+	void	*map;
+	void	*mlx;
+	void	*win;
+
+	if (argc != 2)
+		return (ft_putendl_fd("Usage: ./so_long <ber_file_name>", 1), 0);
+	(void)map;
+	(void)argv;
 	mlx = mlx_init();
 	if (!mlx)
-	{
-		ft_putendl_fd("Error: Failed to initialize mlx.", 2);
-		return (2);
-	}
+		return (ft_putendl_fd("Error: Failed to initialize mlx.", 2), 2);
+	win = mlx_new_window(mlx, 200, 200, "Test MLX");
+	if (!win)
+		return (ft_putendl_fd("Error: Failed to open a window.", 2), 3);
+	mlx_key_hook(win, key_hook, NULL);
+	mlx_mouse_hook(win, mouse_hook, NULL);
+	mlx_expose_hook(win, expose_hook, NULL);
+	mlx_loop_hook(win, loop_hook, NULL);
+	mlx_loop(mlx);
 }
